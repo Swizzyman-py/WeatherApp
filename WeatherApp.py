ad8e208c863406b6712412c83ab4aac797771648
cdustin <coledustin28@gmail.com>
2022-03-10 22:06:58 -0600

    init

---
+++ WeatherApp.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+def search():
+    pass
+
+app = Tk()
+app.title("Weather App")
+app.geometry('700x350')
+
+
+city_text = StringVar()
+city_entry = Entry(app, textvariable=city_text)
+city_entry.pack()
+
+search_btn = Button(app, text='Search Weather',width=12,command=search)
+search_btn.pack()
+
+location_lbl = Label(app, text='Location', font=('bold', 20))
+location_lbl.pack()
+
+image = Label(app, bitmap='')
+image.pack()
+
+temp_lbl = Label(app, text='Temperature')
+temp_lbl.pack()
+
+weather_lbl = Label(app, text='weather')
+weather_lbl.pack()
+
+
+app.mainloop()
